156f3a1afba6c30fea8a4f99a85b6eadc7033cd7
YojiAoki <aoyou65@gmail.com>
2023-10-05 00:33:21 +0900

    型エラーを修正した。

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,20 +1,29 @@
 import NextAuth from 'next-auth';
-import type { NextAuthOptions } from 'next-auth';
-
+import type { NextAuthOptions, User } from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
-
 import { auth } from '@/firebase/admin';
+
+// credentials のスキーマを定義します
+const credentialsSchema = {
+  idToken: { label: 'ID Token', type: 'text' },
+};
 
 export const authOptions: NextAuthOptions = {
   providers: [
     CredentialsProvider({
-      credentials: {},
-      authorize: async ({ idToken }: any, _req) => {
+      credentials: credentialsSchema,
+      authorize: async (credentials: Record<string, string> | undefined, _req) => {
+        const idToken = credentials ? credentials.idToken : undefined;
         if (idToken) {
           try {
             const decoded = await auth.verifyIdToken(idToken);
-
-            return { ...decoded };
+            return {
+              id: decoded.uid,
+              name: decoded.name || '',
+              email: decoded.email || '',
+              image: '', // 画像URLが利用可能であれば追加
+              ...decoded,
+            };
           } catch (err) {
             console.error(err);
           }
@@ -28,9 +37,16 @@ export const authOptions: NextAuthOptions = {
   },
   callbacks: {
     async jwt({ token, user }) {
-      return { ...token, ...user };
+      // 既存のトークンとユーザー情報を組み合わせて新しいトークンを作成
+      const newToken = {
+        ...token,
+        uid: user?.id || token.uid,
+        // user オブジェクトではなく token オブジェクトから emailVerified プロパティを取得
+        emailVerified: token.emailVerified,
+      };
+      // JWT型に合致することを確認して新しいトークンを返す
+      return newToken;
     },
-    // sessionにJWTトークンからのユーザ情報を格納
     async session({ session, token }) {
       session.user.emailVerified = token.emailVerified;
       session.user.uid = token.uid;
