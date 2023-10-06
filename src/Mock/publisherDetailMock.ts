e30fa3af999dcfd97fa849cadc89dd4d55613b60
Dai-xx <118973573+Dai-xx@users.noreply.github.com>
2023-10-07 01:06:01 +0900

    Fix: 投稿者のデータモックを作成

---
+++ src/Mock/publisherDetailMock.ts
@@ -0,0 +1,17 @@
+export const publisherDetailMock = [
+    {
+        name: 'Publisher 1',
+        id: '@aaaaa',
+        date: '2020/01/01 12:12',
+    },
+    {
+        name: 'Publisher 2',
+        id: '@aaaaa',
+        date: '2020/01/01 12:12',
+    },
+    {
+        name: 'Publisher 3',
+        id: '@aaaaa',
+        date: '2020/01/01 12:12',
+    },
+];
