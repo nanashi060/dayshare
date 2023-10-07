a9fcaa04d0f418b59d00bf97e861edfc3dad4b77
Dai-xx <118973573+Dai-xx@users.noreply.github.com>
2023-10-07 21:40:58 +0900

    Fix: 投稿者コンポーネントを微修正

--- src/components/PieChart/PublisherDetail.tsx
+++ src/components/PieChart/PublisherDetail.tsx
@@ -8,11 +8,11 @@ export const Publisher: FC<Prop> = ({ data }) => {
     const id = data.id;
     const time = data.date;
     return (
-        <div className="flex">
+        <div className="flex gap-x-3">
             <Image src={''} alt="" width={42} height={42} style={{ objectFit: 'cover' }} />
 
             <div>
-                <div className="flex">
+                <div className="flex gap-x-3">
                     <p className="text-xs font-normal ">{name}</p>
                     <p className="text-xs font-normal text-[#5F5F5F]">{id}</p>
                 </div>
