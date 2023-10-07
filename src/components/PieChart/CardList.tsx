53e589221c4ccb103469c0f9349f8bd3d148c5d4
Dai-xx <118973573+Dai-xx@users.noreply.github.com>
2023-10-07 22:29:30 +0900

    Fix: 位置微修正

--- src/components/PieChart/CardList.tsx
+++ src/components/PieChart/CardList.tsx
@@ -4,7 +4,7 @@ import { Card } from './Card';
 export const CardList = () => {
     const publisherData = publisherDetailMock;
     return (
-        <div className="grid-cols-3 mx-auto w-[700px] grid place-items-center">
+        <div className="grid-cols-3 mx-auto w-[700px] grid gap-y-10 place-items-center">
             {publisherData.map((item, index) => (
                 <Card key={index} publisherData={item} />
             ))}
