475091d92c3c9d8a8fcd5f56e3db776fdaf7e21c
minamism <greenmint2001@icloud.com>
2023-10-12 04:15:38 +0900

    モーダル部分のデザイン、Firebaseから情報を取得するコードを書こうとした

--- src/components/ProfileTop/index.tsx
+++ src/components/ProfileTop/index.tsx
@@ -5,6 +5,7 @@ import React, { useState, useEffect } from 'react';
 import Modal from 'react-modal';
 import { useSession } from 'next-auth/react';
 import { getStorage, ref, getDownloadURL } from 'firebase/storage';
+import { getFirestore, doc, getDoc } from 'firebase/firestore';
 import '../../../src/src/app/globals.css';
 
 const customStyles: ReactModal.Styles = {
@@ -31,21 +32,40 @@ export const ProfileTop: React.FC = () => {
     const { data: session } = useSession();
     const [modalIsOpen, setModalIsOpen] = useState(false);
     const [imageUrl, setImageUrl] = useState('');
-    // const [userName, setUserName] = useState('');
-    // const [userId, setUserId] = useState('');
-    // const [description, setDescription] = useState('');
+    const [userName, setUserName] = useState('');
+    const [description, setDescription] = useState('');
+    const [editMode, setEditMode] = useState(false); // 編集モードのフラグ
 
     useEffect(() => {
         if (session) {
-            const storage = getStorage();
-            const imagePath = `userImages/${session.user.uid}/profile_picture.png`;
-            const storageRef = ref(storage, imagePath);
-            getDownloadURL(storageRef)
-                .then((url) => {
-                    setImageUrl(url);
+            // Firestoreからユーザー情報を取得
+            const db = getFirestore();
+            const userDocRef = doc(db, 'users', session.user.uid);
+
+            getDoc(userDocRef)
+                .then((docSnapshot) => {
+                    if (docSnapshot.exists()) {
+                        const userData = docSnapshot.data();
+                        // Firestoreから取得したデータをステート変数にセット
+                        setUserName(userData.userName || '');
+                        setDescription(userData.description || '');
+
+                        const storage = getStorage();
+                        const imagePath = `userImages/${session.user.uid}/profile_picture.png`;
+                        const storageRef = ref(storage, imagePath);
+                        getDownloadURL(storageRef)
+                            .then((url) => {
+                                setImageUrl(url);
+                            })
+                            .catch((error) => {
+                                console.error(error);
+                            });
+                    } else {
+                        console.error('ユーザーのドキュメントが存在しません。');
+                    }
                 })
                 .catch((error) => {
-                    console.error(error);
+                    console.error('Firestoreデータ取得エラー:', error);
                 });
         }
     }, [session]);
@@ -54,15 +74,36 @@ export const ProfileTop: React.FC = () => {
         setModalIsOpen(true);
     };
 
-    const closeModal = () => {
+    const closeAndSaveModal = () => {
         setModalIsOpen(false);
+        // ここでフォームの値を保存するロジックを実行
     };
+
+    const cancelEdit = () => {
+        setEditMode(false); // 編集モードを無効にする
+    };
+
+    // プロフィール情報を取得する関数（仮定）
+    const fetchUserProfileData = () => {
+        // ユーザープロフィール情報をデータベースから取得
+        // 取得した情報をもとに初期値を設定
+        const userProfileData = getUserProfileData(); // 仮定の関数
+        setUserName(userProfileData.userName);
+        setDescription(userProfileData.description);
+    };
+
+    useEffect(() => {
+        if (session) {
+            // セッションがある場合にプロフィール情報を取得
+            fetchUserProfileData();
+        }
+    }, [session]);
 
     const data = {
         image: '',
         name: '山田花子',
         id: '@yamahana',
-        discription:
+        description:
             '高校生でエンジニアの山田花子です。やまはなってよんでね!aaaaaaaaaaaaaaaaaaaaaaaaaa',
         followNum: 98,
         followerNum: 202,
@@ -70,15 +111,15 @@ export const ProfileTop: React.FC = () => {
 
     return (
         <div className="mx-[30px]">
-                <button
-                    onClick={openModal}
-                    className="text-gray-5F ml-auto relative block mr-[5vw] top-12 text-xs font-normal hover:bg-[#b9b9b9] text-center bg-gray-D9 py-1 px-2 rounded-[10px]"
-                >
-                    プロフィールを編集
-                </button>
+            <button
+                onClick={openModal}
+                className="text-gray-5F ml-auto relative block mr-[5vw] top-12 text-xs font-normal hover:bg-[#b9b9b9] text-center bg-gray-D9 py-1 px-2 rounded-[10px]"
+            >
+                プロフィールを編集
+            </button>
             <Modal
                 isOpen={modalIsOpen}
-                onRequestClose={closeModal}
+                onRequestClose={closeAndSaveModal}
                 style={customStyles}
                 contentLabel="Profile Modal"
             >
@@ -86,8 +127,36 @@ export const ProfileTop: React.FC = () => {
                     <img
                         src={imageUrl}
                         alt="Profile"
-                        className="w-20 h-20 rounded-full object-cover mr-4"
+                        className="w-24 h-24 rounded-full object-cover mt-5 mb-2"
                     />
+                </div>
+                <form className="text-center">
+                    <label>
+                    <span className='text-sm text-gray-5F mr-56'>Name<br /></span>
+                        <input
+                            type="text"
+                            value={userName}
+                            onChange={(e) => setUserName(e.target.value)}
+                            className="font-normal text-center text-base text-black border-gray-D9 border rounded-md p-1 mb-5 w-3/4"
+                        />
+                    </label>
+                    <br />
+
+                    <label>
+                        <span className='text-sm text-gray-5F mr-48'>Description<br /></span>
+
+                        <textarea
+                            value={description}
+                            onChange={(e) => setDescription(e.target.value)}
+                            className="text-black text-center font-normal text-base resize-none border-gray-D9 border rounded-md p-1 mb-5 w-3/4"
+                        />
+                    </label>
+                </form>
+                <div className='text-center'>
+                    <button onClick={closeAndSaveModal} className="py-2 px-4 mx-3 my-4 bg-kusumi-pink hover:bg-[#a87878] text-white rounded-md">
+                        保存
+                    </button>
+                    <button onClick={cancelEdit} className="py-2 px-4 mx-3 my-4 bg-[#8091BE] hover:bg-[#6F82A9] text-white rounded-md">キャンセル</button>
                 </div>
             </Modal>
             <Image
@@ -103,7 +172,7 @@ export const ProfileTop: React.FC = () => {
                 {data.id}
             </p>
             <p className="text-gray-5F text-center font-normal text-base pt-5">
-                {data.discription}
+                {data.description}
             </p>
             <div className="flex justify-center text-black text-center font-normal pt-6 text-base gap-x-5">
                 <p>フォロー中 {data.followNum}人</p>
