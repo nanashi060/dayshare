73231ec2725946acc21b3900b65499348335dc4b
YojiAoki <aoyou65@gmail.com>
2023-10-15 00:26:17 +0900

    名前とidを表示するように

--- src/components/PieChart/PublisherDetail.tsx
+++ src/components/PieChart/PublisherDetail.tsx
@@ -5,22 +5,38 @@ import { formatDate } from 'utils/formatDate';
 import { onAuthStateChanged } from 'firebase/auth';
 import { getStorage, ref, getDownloadURL } from 'firebase/storage';
 import { auth } from '../../firebase/client';
+import axios from 'axios';
+import useSWR from 'swr';
 
-type Prop = { data: any };
+type Prop = {
+    data: {
+        userId: string;
+        timestamp: {
+            _seconds: number;
+        };
+        icon_image?: string;
+    };
+};
 
 export const Publisher: React.FC<Prop> = ({ data }) => {
-    const date = new Date(data.timestamp._seconds * 1000);
+    const { userId, timestamp, icon_image } = data;
+    const date = new Date(timestamp._seconds * 1000);
     const formatTime = formatDate(date);
+    const [imageUrl, setImageUrl] = useState(icon_image || '/default.png');
+    const fetcher = (url: string) => axios.get(url).then((res) => res.data);
+    const { data: profileData }: any = useSWR(`/api/profileData/${userId}`, fetcher);
 
-    const [imageUrl, setImageUrl] = useState(data.icon_image || '/default.png');
+    console.log(profileData);
 
-    const userId = data.userId;
+    const name = profileData?.name;
+    const id = profileData?.id;
 
     useEffect(() => {
         if (!userId) return;
 
         const storage = getStorage();
         const imagePath = `userImages/${userId}/profile_picture.png`;
+
         onAuthStateChanged(auth, (user) => {
             if (user) {
                 const storageRef = ref(storage, imagePath);
@@ -28,7 +44,6 @@ export const Publisher: React.FC<Prop> = ({ data }) => {
                     .then(setImageUrl)
                     .catch((error) => {
                         if (error.code === 'storage/object-not-found') {
-                            // ファイルが存在しない場合、デフォルトの画像をセットする
                             setImageUrl('/default.png');
                         }
                     });
@@ -47,11 +62,10 @@ export const Publisher: React.FC<Prop> = ({ data }) => {
                 height={42}
                 className="rounded-full object-cover"
             />
-
-            <div>
+            <div className="flex gap-x-3 flex-col">
                 <div className="flex gap-x-3">
-                    <p className="text-xs font-normal">{data.name}</p>
-                    <p className="text-xs font-normal text-[#5F5F5F]">{data.id}</p>
+                    <p className="text-xs font-normal">{name}</p>
+                    <p className="text-xs font-normal text-[#5F5F5F]">{id}</p>
                 </div>
                 <p className="text-xs font-normal text-[#5F5F5F]">{formatTime}</p>
             </div>
