b39e563e60cd00c79263ee91af0a5f210e7fa2ca
YojiAoki <aoyou65@gmail.com>
2023-11-22 03:15:44 +0900

    ヘッダー画像置き場所変更

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,7 +4,7 @@ export const Header = () => {
     return (
         <div className="bg-white flex justify-center py-2 my-3 z-50 opacity-[97%]">
             <img
-                src={'https://firebasestorage.googleapis.com/v0/b/dayshare-392fb.appspot.com/o/dayshare.png?alt=media&token=ff7f5b93-d0bc-4c28-b1e5-cfc3cac1687b&_gl=1*159urxu*_ga*MzAxOTE5NTcuMTY5NjI1MzgyOQ..*_ga_CW55HF8NVT*MTY5NzMyNTc3Ny4zNC4xLjE2OTczMjU4OTcuNDYuMC4w'}
+                src={'https://firebasestorage.googleapis.com/v0/b/dayshare-b38a8.appspot.com/o/file-ag266kCk07M5sePBcgz54r3C-removebg-preview.png?alt=media&token=4bb1ef5b-688f-42f0-8eb4-00750ae2e1ff'}
                 alt="DayShare-logo"
                 width={80}
                 height={80}
